1c0d7b43f75dd63b69530d16ba95d0bd3b371c44
alphonsotran <alphonsotran@gmail.com>
2018-05-04 14:23:39 +0200

    Add marker and infowindow. Test demo run successfully.

--- src/components/MapsContainer.js
+++ src/components/MapsContainer.js
@@ -2,20 +2,51 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapsContainer extends Component {
-  render() {
-    const style = {
-        width: '90vw',
-        height: '75vh'
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+        };
+
+    onMarkerClick = (props, marker, e) =>
+    this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+    });
+
+    onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+        this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+        })
     }
+    };
+    render() {
+        const style = {
+            width: '90vw',
+            height: '75vh'
+        }
     return (
-      <Map 
-        google={this.props.google} 
+    <Map 
+        google={this.props.google}
+        onClick={this.onMapClicked} 
         zoom={14} 
         style={style}>
         <Marker
+            onClick={this.onMarkerClick}
             title={'The marker`s title will appear as a tooltip.'}
             name={'SOMA'}
-            position={{lat: 37.778519, lng: -122.405640}} />    
+            position={{lat: 37.778519, lng: -122.405640}} /> 
+
+        <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+            <div>
+                <h2>{this.state.selectedPlace.name}</h2>
+            </div>
+        </InfoWindow>   
         </Map>
     );
   }
