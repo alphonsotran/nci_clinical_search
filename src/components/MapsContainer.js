d41d451d1c666328b7841ffc45228ce3a77bf865
alphonsotran <alphonsotran@gmail.com>
2018-05-04 13:44:18 +0200

    Adjust map to smaller size and responsive.

--- src/components/MapsContainer.js
+++ src/components/MapsContainer.js
@@ -3,8 +3,16 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapsContainer extends Component {
   render() {
+    const style = {
+        width: '90vw',
+        height: '75vh'
+    }
     return (
-      <Map google={this.props.google} zoom={14} />
+      <Map 
+        google={this.props.google} zoom={14} 
+        style={style}
+        
+        />
     );
   }
 }
