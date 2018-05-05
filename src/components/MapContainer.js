4c040247f7941a45107f194dcb734881df07e02f
alphonsotran <alphonsotran@gmail.com>
2018-05-05 17:44:13 +0200

    Add constructor and super to MapContainer Class.

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,22 +3,21 @@ import ReactDOM from 'react-dom'
 
 
 export default class MapContainer extends Component {
-
-  // ======================
-  // ADD LOCATIONS TO STATE
-  // ======================
-  state = {
-    locations: [
-      { name: "New York County Supreme Court", location: {lat: 40.7143033, lng: -74.0036919} },
-      { name: "Queens County Supreme Court", location: {lat: 40.7046946, lng: -73.8091145} },
-      { name: "Kings County Supreme Court", location: {lat: 40.6940226, lng: -73.9890967} },
-      { name: "Richmond County Supreme Court", location: {lat: 40.6412336, lng: -74.0768597} },
-      { name: "Bronx Supreme Court", location: {lat: 40.8262388, lng: -73.9235238} }
-    ]
-  }
+    constructor(props){
+        super(props)
+        this.state = {
+          locations: [
+            { name: "New York County Supreme Court", location: {lat: 40.7143033, lng: -74.0036919} },
+            { name: "Queens County Supreme Court", location: {lat: 40.7046946, lng: -73.8091145} },
+            { name: "Kings County Supreme Court", location: {lat: 40.6940226, lng: -73.9890967} },
+            { name: "Richmond County Supreme Court", location: {lat: 40.6412336, lng: -74.0768597} },
+            { name: "Bronx Supreme Court", location: {lat: 40.8262388, lng: -73.9235238} }
+          ]
+        }
+    }
 
   componentDidMount() {
-    this.loadMap(); // call loadMap function to load the google map
+    this.loadMap(); 
   }
 
   loadMap() {
