457d9c321a39db56178d69ab6ce7b66494ba9db3
alphonsotran <alphonsotran@gmail.com>
2018-05-14 17:31:07 +0200

    Reduce number of clinical sites to 20.

--- src/components/Site.js
+++ src/components/Site.js
@@ -5,13 +5,14 @@ import { GoogleApiWrapper } from 'google-maps-react';
 const Site = (props) => {
   console.log("Site", props.location.state.USstate)
   
-  //uses JSON data to filter by state and truthy 
+  //uses JSON data to filter by state and truthy and cuts down cliincal site to 20
   const filterStates =
     props.location.state.clinic.filter((el) => {
       return el.org_state_or_province === props.location.state.USstate && el.org_coordinates
     })
+    .slice(0, 20)
 
-  const filteredLocations = filterStates.map((el, idx)=>(
+  const filteredLocations = filterStates.map((el) => (
     { name: el.contact_name, 
       location: {
         lat: el.org_coordinates.lat,
