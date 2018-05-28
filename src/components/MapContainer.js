543907b5e8c0c6baa9cb658814d4d52e607e1509
alphonsotran <alphonsotran@gmail.com>
2018-05-28 02:02:08 +0200

    Remove comments and semicolons.

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 
@@ -10,43 +10,37 @@ export default class MapContainer extends Component {
         }
     }
   
-    
     componentDidMount() {
-      this.loadMap(); 
+      this.loadMap() 
     }
     
     loadMap() {
       const center = this.state.locations[0].location
-    if (this.props && this.props.google) { // checks to make sure that props have been passed
-      const {google} = this.props; // sets props equal to google
-      const maps = google.maps; // sets maps to google maps props
-
-      const mapRef = this.refs.map; // looks for HTML div ref 'map'. Returned in render below.
-      const node = ReactDOM.findDOMNode(mapRef); // finds the 'map' div in the React DOM, names it node
-
-      const mapConfig = Object.assign({}, {
-        center: center, // sets center of google map to NYC.
-        zoom: 7, // sets zoom. Lower numbers are zoomed further out.
-        mapTypeId: 'roadmap' // optional main map layer. Terrain, satellite, hybrid or roadmap--if unspecified, defaults to roadmap.
+      if (this.props && this.props.google) { 
+        const {google} = this.props 
+        const maps = google.maps 
+        const mapRef = this.refs.map 
+        const node = ReactDOM.findDOMNode(mapRef) 
+        const mapConfig = Object.assign({}, {
+          center: center, 
+          zoom: 7, 
+          mapTypeId: 'roadmap'
       })
 
-      this.map = new maps.Map(node, mapConfig); // creates a new Google map on the specified node (ref='map') with the specified configuration set above.
+      this.map = new maps.Map(node, mapConfig) 
 
-  // ==================
-  // ADD MARKERS TO MAP
-  // ==================
-      this.state.locations.forEach( location => { // iterate through locations saved in state
-        const marker = new google.maps.Marker({ // creates a new Google maps Marker object.
-          position: {lat: location.location.lat, lng: location.location.lng}, // sets position of marker to specified location
-          map: this.map, // sets markers to appear on the map we just created on line 35
-          title: location.organization // the title of the marker is set to the name of the location
-        });
+      this.state.locations.forEach( location => { 
+        const marker = new google.maps.Marker({ 
+          position: {lat: location.location.lat, lng: location.location.lng}, 
+          map: this.map, 
+          title: location.organization 
+        })
         var infowindow = new google.maps.InfoWindow({
-          content: `<h4>${marker.title}</h4><p>${location.streetAddress}</p><p>${location.city},&nbsp;${location.USstate}</p>`
-        });
+          content: `<h4>${marker.title}</h4><p>${location.streetAddress}</p><p>${location.city},&nbsp${location.USstate}</p>`
+        })
         marker.addListener('click', function() {
-          infowindow.open(this.map, marker);
-        });
+          infowindow.open(this.map, marker)
+        })
       })
 
     }
@@ -54,12 +48,11 @@ export default class MapContainer extends Component {
 
   render() {
     console.log("MapContainer Filtered", this.state.locations)
-    const style = { // MUST specify dimensions of the Google map or it will not work. Also works best when style is specified inside the render function and created as an object
-      width: '90vw', // 90vw basically means take up 90% of the width screen. px also works.
-      height: '75vh' // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
+    const style = { 
+      width: '90vw', 
+      height: '75vh' 
     }
-
-    return ( // in our return function you must return a div with ref='map' and style.
+    return (
       <div ref="map" style={style}>
         loading map...
       </div>
